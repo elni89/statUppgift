4addc0a19f184485a92716ed7a903436d6641927
elni89 <niklas.svensson.webd23@edu.tucsweden.se>
2024-02-22 20:35:02 +0100

    added code progarm.cs

---
+++ program.cs
@@ -0,0 +1,9 @@
+using Statistics;
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Statistics.Statistics.DescriptiveStatistics());
+        Console.ReadKey();
+    }
+}
